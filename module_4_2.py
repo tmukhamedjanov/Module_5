3d3760d4030ebf599650b94cc4b53298267cbc18
timur <t.mukhamedjanov@mail.ru>
2025-01-19 21:06:16 +0400

    1

--- module_4_2.py
+++ module_4_2.py
@@ -1,1 +1,7 @@
-12312
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+    return
+test_function()
+#inner_function() Мы не можем вызвать эту функцию, так как она находится в другой области видимости
